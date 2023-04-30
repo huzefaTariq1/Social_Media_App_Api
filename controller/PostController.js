2d7c5eb9b2247437cf04342ce27f6cc7ad60d994
huzefaTariq1 <101399969+huzefaTariq1@users.noreply.github.com>
2023-04-30 12:31:37 +0100

    Addde controller Function of Like And Unlike ,Delete Post
    Also Handles deleted post from users's posts array

--- controller/PostController.js
+++ controller/PostController.js
@@ -31,6 +31,92 @@ const createPost = async (req, res) => {
 
 }
 
+
+const deletePost=async(req,res)=>{
+try {
+
+   const post=await Post.findById(req.params.id)
+
+   if (!post){
+    return res.status(404).json({
+      success:false,
+      message:"No Post Found"
+    })
+   }
+
+   if (post.owner.toString()!==req.user.id.toString()){
+     return res.status(401).json({
+      success:false,
+      message:"UnAuthorized"
+     })
+   }
+ 
+   await Post.findByIdAndDelete(req.params.id)
+
+   // getting user
+   const user=await User.findById(req.user.id)
+   // removing post from user array
+   const index=user.posts.indexOf(req.params.id)
+
+   user.posts.splice(index,1)
+
+   await user.save()
+
+   res.status(200).json({
+    success:true,
+    message:"Deleted"
+   })
+
+} catch (error) {
+  res.status(500).json({
+    success: false,
+    message: error.message
+  })
+}
+}
+
+const likeAndUnlike=async(req,res)=>{
+  try {
+  
+     const post=await Post.findById(req.params.id)
+  
+     if (!post){
+      return res.status(404).json({
+        success:false,
+        message:"No Post Found"
+      })
+     }
+
+     if (post.likes.includes(req.user.id)){
+      const index=post.likes.indexOf(req.user.id);
+      post.likes.splice(index,1);
+
+      await post.save();
+      return res.status(200).json({
+        success:true,
+        message:"Post Unliked"
+      })
+     }
+
+     post.likes.push(req.user.id)
+
+     await post.save()
+
+     res.status(200).json({
+      success:true,
+      message:"Post Liked"
+     })
+  
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+  }
+
 module.exports = {
-  createPost
+  createPost,
+  deletePost,
+  likeAndUnlike
 }
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,24 @@
 const router=require("express").Router();
-const {createPost}=require("../controller/PostController")
+const {createPost, deletePost, likeAndUnlike}=require("../controller/PostController")
 const auth=require("../middleware/authMiddleware")
 
-// @route     /api/user
-// @desc       creating user
-// access     public
+// @route     /api/post
+// @desc       creating post
+// access     private
 
 router.post("/",auth,createPost)
+
+// @route     /api/post/:id
+// @desc      deleting post
+// access     private
+
+router.delete("/:id",auth,deletePost)
+
+// @route     /api/post/:id
+// @desc      like and unlike
+// access     get
+
+router.get("/:id",auth,likeAndUnlike)
 
 
 
